171be46056c4825c6331083e031b476ad2652ba6
FATHIMA IRFANA N P <139485504+fathima-irfana-np@users.noreply.github.com>
2025-11-11 16:45:05 +0530

    Add files via upload

--- cognitive_captcha/cognitive_captcha/urls.py
+++ cognitive_captcha/cognitive_captcha/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+import os
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('captcha.urls')),
+    path('animations/<path:path>', serve, {'document_root': os.path.join(settings.BASE_DIR, 'animations')}),
 ]
+# THIS PART IS CRITICAL FOR MEDIA FILES
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
